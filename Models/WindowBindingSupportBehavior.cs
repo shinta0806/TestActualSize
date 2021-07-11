4603cd8c788dafc2d1ddfa4f0a192d9be7c86456
shinta0806 <shinta.0806@gmail.com>
2021-07-11 17:04:11 +0900

    Ver 1.00 時点

---
+++ Models/WindowBindingSupportBehavior.cs
@@ -0,0 +1,112 @@
+﻿// ============================================================================
+// 
+// Window のバインド可能なプロパティーを増やすためのビヘイビア
+// 
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// 
+// ----------------------------------------------------------------------------
+
+using Microsoft.Xaml.Behaviors;
+
+using System;
+using System.Windows;
+
+namespace TestActualSize.Models
+{
+	public class WindowBindingSupportBehavior : Behavior<Window>
+	{
+		// ====================================================================
+		// public プロパティー
+		// ====================================================================
+
+		// FrameworkElement.ActualWidth をバインド可能にする
+		public Double ActualWidth
+		{
+			get => (Double)GetValue(ActualWidthProperty);
+			set => SetValue(ActualWidthProperty, value);
+		}
+
+		// Window.IsActive をバインド可能にする
+		public Boolean IsActive
+		{
+			get => (Boolean)GetValue(IsActiveProperty);
+			set => SetValue(IsActiveProperty, value);
+		}
+
+		// ====================================================================
+		// public メンバー変数
+		// ====================================================================
+
+		// FrameworkElement.ActualWidth をバインド可能にする
+		public static readonly DependencyProperty ActualWidthProperty =
+				DependencyProperty.Register(nameof(ActualWidth), typeof(Double), typeof(WindowBindingSupportBehavior),
+				new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+		// Window.IsActive をバインド可能にする
+		public static readonly DependencyProperty IsActiveProperty =
+				DependencyProperty.Register(nameof(IsActive), typeof(Boolean), typeof(WindowBindingSupportBehavior),
+				new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, SourceIsActiveChanged));
+
+		// ====================================================================
+		// protected メンバー関数
+		// ====================================================================
+
+		// --------------------------------------------------------------------
+		// アタッチ時の準備作業
+		// --------------------------------------------------------------------
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+
+			AssociatedObject.Activated += ControlActivated;
+			AssociatedObject.Deactivated += ControlDeactivated;
+			AssociatedObject.SizeChanged += ControlSizeChanged;
+		}
+
+		// ====================================================================
+		// private メンバー関数
+		// ====================================================================
+
+		// --------------------------------------------------------------------
+		// View 側で IsActive が変更された
+		// --------------------------------------------------------------------
+		private void ControlActivated(Object? sender, EventArgs args)
+		{
+			IsActive = true;
+		}
+
+		// --------------------------------------------------------------------
+		// View 側で IsActive が変更された
+		// --------------------------------------------------------------------
+		private void ControlDeactivated(Object? sender, EventArgs args)
+		{
+			IsActive = false;
+		}
+
+		// --------------------------------------------------------------------
+		// View 側で Size が変更された
+		// --------------------------------------------------------------------
+		private void ControlSizeChanged(Object? sender, EventArgs args)
+		{
+			ActualWidth = AssociatedObject.ActualWidth;
+		}
+
+		// --------------------------------------------------------------------
+		// ViewModel 側で IsActive が変更された
+		// --------------------------------------------------------------------
+		private static void SourceIsActiveChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+		{
+			if ((obj is not WindowBindingSupportBehavior thisObject) || thisObject.AssociatedObject == null)
+			{
+				return;
+			}
+
+			if ((Boolean)args.NewValue)
+			{
+				thisObject.AssociatedObject.Activate();
+			}
+		}
+	}
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,21 +1,112 @@
-﻿using Livet;
+﻿// ============================================================================
+// 
+// メインウィンドウの ViewModel
+// 
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// ビヘイビアのテスト用プログラム
+// ----------------------------------------------------------------------------
+
+using Livet;
 using Livet.Commands;
-using Livet.EventListeners;
-using Livet.Messaging;
-using Livet.Messaging.IO;
-using Livet.Messaging.Windows;
+
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using TestActualSize.Models;
+using System.Threading.Tasks;
 
 namespace TestActualSize.ViewModels
 {
 	public class MainWindowViewModel : ViewModel
 	{
-		// Some useful code snippets for ViewModel are defined as l*(llcom, llcomn, lvcomm, lsprop, etc...).
+		// ====================================================================
+		// public プロパティー
+		// ====================================================================
+
+		// --------------------------------------------------------------------
+		// View 通信用のプロパティー
+		// --------------------------------------------------------------------
+
+		// ウィンドウタイトル
+		private String _title = "ビヘイビアのテスト";
+		public String Title
+		{
+			get => _title;
+			set => RaisePropertyChangedIfSet(ref _title, value);
+		}
+
+		// 設定幅
+		private Double _width = 500.0;
+		public Double Width
+		{
+			get => _width;
+			set => RaisePropertyChangedIfSet(ref _width, value);
+		}
+
+		// 実際の幅
+		private Double _actualWidth;
+		public Double ActualWidth
+		{
+			get => _actualWidth;
+			set => RaisePropertyChangedIfSet(ref _actualWidth, value);
+		}
+
+		// 設定高さ
+		private Double _height = 250.0;
+		public Double Height
+		{
+			get => _height;
+			set => RaisePropertyChangedIfSet(ref _height, value);
+		}
+
+		// 実際の高さ
+		private Double _actualHeight;
+		public Double ActualHeight
+		{
+			get => _actualHeight;
+			set => RaisePropertyChangedIfSet(ref _actualHeight, value);
+		}
+
+		// アクティブ化
+		private Boolean _isActive;
+		public Boolean IsActive
+		{
+			get => _isActive;
+			set => RaisePropertyChangedIfSet(ref _isActive, value);
+		}
+
+		// --------------------------------------------------------------------
+		// コマンド
+		// --------------------------------------------------------------------
+
+		#region アクティブボタンの制御
+		private ViewModelCommand? _buttonActivateClickedCommand;
+
+		public ViewModelCommand ButtonActivateClickedCommand
+		{
+			get
+			{
+				if (_buttonActivateClickedCommand == null)
+				{
+					_buttonActivateClickedCommand = new ViewModelCommand(ButtonActivateClicked);
+				}
+				return _buttonActivateClickedCommand;
+			}
+		}
+
+		public async void ButtonActivateClicked()
+		{
+			await Task.Delay(5000);
+			IsActive = true;
+		}
+		#endregion
+
+		// ====================================================================
+		// public メンバー関数
+		// ====================================================================
+
+		// --------------------------------------------------------------------
+		// 初期化
+		// --------------------------------------------------------------------
 		public void Initialize()
 		{
 		}
